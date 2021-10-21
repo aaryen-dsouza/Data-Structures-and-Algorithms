f8ab5e715e94b1bdf4ab16e11ea00a287ddbdf12
Aaryen Cyril D'souza <dc.aaryen@gmail.com>
2021-10-21 21:20:10 +0530

    added FMPCS

---
+++ Problems/src/FIrstMissingPositiveCS.java
@@ -0,0 +1,29 @@
+public class FIrstMissingPositiveCS {
+    public static void main(String[] args) {
+        int[] nums = {3,4,-1,1};
+        int i = 0;
+        while(i < nums.length){
+            if (nums[i] <= 0 || nums[i] > nums.length) {
+                i++;
+                continue;
+            }
+            int current = nums[i] - 1;
+            if(nums[current] != nums[i]){
+                swap(nums, i, current);
+            } else {
+                i++;
+            }
+        }
+        for (i = 0; i < nums.length; i++){
+            if(nums[i] != i + 1){
+                System.out.println(i + 1);
+            }
+        }
+//        System.out.println(nums.length + 1);
+    }
+    static void swap(int[] nums,int first,int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
