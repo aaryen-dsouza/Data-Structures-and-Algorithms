c5074ed69e7f5178e23ac9802d5fe61096189432
Aaryen Cyril D'souza <dc.aaryen@gmail.com>
2021-12-08 00:30:54 +0530

    added bitwise probs

---
+++ Problems/src/PascalRowSum.java
@@ -0,0 +1,7 @@
+public class PascalRowSum {
+    public static void main(String[] args) {
+        int row = 3;
+        int ans = 1 << (row - 1);
+        System.out.println(ans);
+    }
+}
---
+++ Problems/src/PowOfTwo.java
@@ -0,0 +1,11 @@
+public class PowOfTwo {
+    public static void main(String[] args) {
+        int n = 0;
+        if(n == 0) {
+            System.out.println(false);
+        } else {
+            boolean ans = (n & (n - 1)) == 0;
+            System.out.println(ans);
+        }
+    }
+}
